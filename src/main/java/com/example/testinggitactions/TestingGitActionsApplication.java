98a87209d2d40e2238553219d6bfda0f23a5f678
Davit012 <jandavjan5252@mail.ru>
2022-03-25 16:01:27 +0400

    Initial commit

---
+++ src/main/java/com/example/testinggitactions/TestingGitActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testinggitactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingGitActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingGitActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testinggitactions/TestingGitActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testinggitactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingGitActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
