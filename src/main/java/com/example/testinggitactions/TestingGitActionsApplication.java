70b5586d51f954f78d186506c9b11f7bedc31054
Davit012 <jandavjan5252@mail.ru>
2022-04-07 13:48:13 +0400

    588

--- src/main/java/com/example/testinggitactions/TestingGitActionsApplication.java
+++ src/main/java/com/example/testinggitactions/TestingGitActionsApplication.java
@@ -10,6 +10,7 @@ public class TestingGitActionsApplication {
         SpringApplication.run(TestingGitActionsApplication.class, args);
     }
     public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(TestingGitActionsApplication.class, args);
     }
 
