dc75bd7c07edba9c528444e40bd725cdfd7cb4b4
Davit012 <jandavjan5252@mail.ru>
2022-04-07 16:04:05 +0400

    58844

--- src/main/java/com/example/testinggitactions/TestingGitActionsApplication.java
+++ src/main/java/com/example/testinggitactions/TestingGitActionsApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestingGitActionsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestingGitActionsApplication.class, args);
+        SpringApgfhgjlication.run(TestingGitActionsApplication.class, args);
+    }
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+        SpringApgfhgjlication.run(TestingGitActionsApplication.class, args);
     }
 
 }
